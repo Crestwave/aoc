20c60fa2bd1d5f75cb0c6c8c743c993d55bfcb00
Crestwave <crestwave@users.noreply.github.com>
2022-12-05 23:24:46 +0800

    2022/3: optimize a.sh

--- 2022/3/a.sh
+++ 2022/3/a.sh
@@ -2,12 +2,11 @@
 while read -r line; do
 	a=${line::${#line}/2}
 	b=${line:${#line}/2}
-	c=${a//["${a//["$b"]}"]}
-	c=${c::1}
+	[[ $a =~ [$b] ]]
+	c=${BASH_REMATCH[0]}
 
 	printf -v val %d "'$c"
 
-	# alternatively, use a [[ $c =~ [[:lower:]] check
 	(( val -= (val >= 97 ? 96 : 38)))
 	(( sum += val ))
 done <"$1"
